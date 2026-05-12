74587bda3b1319a11555cc0810888342bcdaca31
Jorge Guerrero <georgeGuerrero904@gmail.com>
2026-05-11 22:00:22 -0500

    adding appsettings to ignore list and installing base packages

---
+++ Models/Database/BaseTableInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace JobsApp.Models.Database
+{
+    public class BaseTableInfo
+    {
+        public int Id { get; set; }
+        public bool isActive { get; set; }
+
+    }
+}
---
+++ Models/Database/JobsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JobsApp.Models.Database
+{
+    public class JobsContext : DbContext
+    {
+
+    }
+}
